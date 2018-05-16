f684a444bb88aeebab4da0f2cbf0d5fb4f7b95a0
Jack Jiang <jackywathy24@gmail.com>
2018-05-16 10:17:53 +1000

    started dynamic form text creator

--- LAMP/frontend/controls/DynamicFormCreation.vb
+++ LAMP/frontend/controls/DynamicFormCreation.vb
@@ -1,3 +1,21 @@
 ﻿Public Class DynamicFormCreation
+    Private Function MakePanel() As Panel
+        Dim newPanel As New Panel
+        Return newPanel
+    End Function
 
+
+    Protected Overrides Sub OnPaddingChanged(e As EventArgs)
+        MyBase.OnPaddingChanged(e)
+
+        UpdatePanels()
+    End Sub
+
+    Public Sub UpdatePanels()
+
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
 End Class
