c7a877e3050d9ce00feb491e57f3e942018a9f26
Jack Jiang <jackywathy24@gmail.com>
2018-09-09 21:33:54 +1000

    removed dups (squash)
    removed definite duplicates (squash)
    
    
    removed toolbar (squash)
    
    
    testinng close box(squash)
    
    
    checkpoint (squash)

--- LampClient/forms/AboutBox.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class AboutBox
-    Private Sub OKButton_Click(sender As Object, e As EventArgs) Handles OKButton.Click
-        Me.Close()
-    End Sub
-
-    Private Sub LogoPictureBox_Click(sender As Object, e As EventArgs) Handles LogoPictureBox.Click
-        DebugOptions.Show()
-    End Sub
-End Class
