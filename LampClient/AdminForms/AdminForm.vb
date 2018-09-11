6c0c09190365d98ee91010c886fce2debd23150a
Shourov Quazi <qshourov@gmail.com>
2018-09-11 23:42:58 +1000

    Added Admin button hotfix
    #proffesional

--- LampClient/AdminForms/AdminForm.vb
+++ LampClient/AdminForms/AdminForm.vb
@@ -1,9 +1,15 @@
 ﻿Public Class AdminForm
-    Private Sub AdminToolbar1_Load(sender As Object, e As EventArgs) 
 
-    End Sub
 
     Private Sub btnStandard_Click(sender As Object, e As EventArgs) Handles btnStandard.Click
         AdminToolbar1.ShowNewForm(Me, HomeForm)
+    End Sub
+
+    Private Sub load() Handles MyBase.Load
+        AdminToolbar1.btnHome.Enabled = False
+        AdminToolbar1.btnApproveTemplates.Enabled = True
+        AdminToolbar1.btnApproveJob.Enabled = True
+        AdminToolbar1.btnManageUsers.Enabled = True
+
     End Sub
 End Class
